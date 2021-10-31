2bd9681382028d0bada215cee468aaccd2d93800
Kabane-UN <60570949+Kabane-UN@users.noreply.github.com>
2021-10-31 14:45:40 +0300

    Update binsearch.c

--- binsearch.c
+++ binsearch.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 unsigned long binsearch(unsigned long nel, int (*compare)(unsigned long i))
 {
+    unsigned long rnel = nel;
     unsigned long nu = 0;
     while (1)
     {
         switch (compare((nu+nel)/2))
         {
             case 1:
+                if (nel == (nu+nel)/2) return rnel;
                 nel=(nu+nel)/2;
                 break;
             case -1:
+                if (nu == (nu+nel)/2) return rnel;
                 nu=(nu+nel)/2;
                 break;
             default:
@@ -19,14 +22,14 @@ unsigned long binsearch(unsigned long nel, int (*compare)(unsigned long i))
     end:
     return (nu+nel)/2;
 }
-int array[5] = {1,2,3,4,5};
+int array[5] = {1,2,4,5,6};
 
 int compare(unsigned long i)
 {
-    if(array[i]>array[0])
+    if(array[i]>5)
     {
         return 1;
-    } else if (array[i]<array[0])
+    } else if (array[i]<5)
     {
         return -1;
     } else
