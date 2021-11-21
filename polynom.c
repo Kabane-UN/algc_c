47d2b1efde317e000f7575a654ae143fe5c0c5b4
Kabane-UN <60570949+Kabane-UN@users.noreply.github.com>
2021-11-21 10:36:58 +0300

    Update polynom.c

--- polynom.c
+++ polynom.c
@@ -2,33 +2,33 @@
 
 int main()
 {
-    long int x0, a, s=0,sp=0;
-    int n;
-    scanf("%d", &n);
-    long int arr[n];
-    scanf("%ld", &x0);
-    scanf("%ld", &a);
-    if (n==0)
-    {
-        printf("%ld\n", a);
-        printf("%d\n", 0);
-        return 0;
-    }
-    s=a;
-    sp=a*n;
-    for(long int i = 0; i < n; i++)
-    {
-        s*=x0;
-        scanf("%ld", &a);
-        s+=a;
-        if ((n-i) > 1)
-        {
-            sp*=x0;
-            sp+=a*(n-i-1);
-        }
-    }
+	long long int x0, a, s=0,sp=0;
+	int n;
+	scanf("%d", &n);
+	long long int arr[n];
+	scanf("%lld", &x0);
+	scanf("%lld", &a);
+	if (n==0)
+	{
+		printf("%ld\n", a);
+		printf("%d\n", 0);
+		return 0;
+	}
+	s=a;
+	sp=a*n;
+	for(long long int i = 0; i < n; i++)
+	{
+		s*=x0;
+		scanf("%ld", &a);
+		s+=a;
+		if ((n-i) > 1)
+		{
+			sp*=x0;
+			sp+=a*(n-i-1);
+		}
+	}
 
-    printf("%ld\n", s);
-    printf("%ld\n", sp);
-    return 0;
+	printf("%lld\n", s);
+	printf("%lld\n", sp);
+	return 0;
 }
